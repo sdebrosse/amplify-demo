f45cc4b4f94bbc6c2b47d39491252d2e9ed2e580
Spencer DeBrosse <sndebrosse@gmail.com>
2021-01-05 20:40:54 -0800

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://k7oivgs4xc.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
